596fad260a5eb4127e2d5bfc816c9ac8155378da
Tawfique <mdtoufiq2342@gamail.com>
2021-11-15 16:26:07 +0600

    A new function Addition added with integer argument and return type

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator {
     public static void main(String[] args) {
+        int a;
+        a = add(5, 10);
+        System.out.println(a);
+    }
 
+    static int add(int a, int b) {
+        return a + b;
     }
 }
